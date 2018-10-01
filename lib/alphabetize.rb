2955726ca1f3e4566384638547dc2dc77f07fa4b
Maayan Kfir <mayankfir@gmail.com>
2018-10-01 21:27:32 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ def alphabetize(arr)
   arr.sort_by do |phrase|
     phrase.split("").collect do |first_letter|
 
-      esperanto_alphabet.sort_by(first_letter)
+      esperanto_alphabet.chars.index(first_letter)
     end
   end
 end
