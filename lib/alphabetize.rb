a045d71195cb1fa19e1637de1c9f14e5f5e49127
Maayan Kfir <mayankfir@gmail.com>
2018-10-01 21:27:44 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.split("").collect do |first_letter|
+
+      esperanto_alphabet.chars.index(first_letter)
+    end
+  end
 end
