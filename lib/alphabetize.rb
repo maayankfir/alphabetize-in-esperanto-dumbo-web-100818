fd38cc54872e5ad38bc73135c0823981f6cd51b2
Maayan Kfir <mayankfir@gmail.com>
2018-10-01 21:25:32 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by do |phrase| 
+  arr.sort_by do |phrase|
     phrase.split("").collect do |first_letter|
       ESPERANTO_ALPHABET.chars.index(first_letter)
     end
